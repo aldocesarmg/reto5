e70025b7429522df92e6d5f67bf215b49c7f7d4b
aldocesarmg <aldocesarmg@gmail.com>
2019-02-22 23:41:59 -0600

    Creacion de la clase TercerPrograma

---
+++ src/reto5/TercerPrograma.java
@@ -0,0 +1,13 @@
+
+package RETO5;
+
+public class TercerPrograma {
+    public double ladoA, ladoB;
+    public boolean esValido;
+    
+    private static double validaLados(double A, double B){
+        return A+B;
+}
+    
+}
+
